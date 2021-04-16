cb862d6b0e8b24e839d18b688c78f9edb5032974
patrickt <torralbapatrick15@gmail.com>
2021-04-15 21:26:23 -0500

    :bug: Fix assets path

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,22 +8,22 @@
 			class="grid grid-flow-row grid-cols-3 gap-3 mx-4 md:grid-cols-4 md:gap-4 lg:grid-cols-5 lg:gap-5"
 		>
 			<div class="img-square">
-				<img src="../static/img/img1.png" alt="Project 1" />
+				<img src="/img/img1.png" alt="Project 1" />
 			</div>
 			<div class="img-square">
-				<img src="../static/img/img2.png" alt="Project 2" />
+				<img src="/img/img2.png" alt="Project 2" />
 			</div>
 			<div class="img-square">
-				<img src="../static/img/img3.png" alt="Project 3" />
+				<img src="/img/img3.png" alt="Project 3" />
 			</div>
 			<div class="img-square">
-				<img src="../static/img/img4.png" alt="Project 4" />
+				<img src="/img/img4.png" alt="Project 4" />
 			</div>
 			<div class="img-square">
-				<img src="../static/img/img5.png" alt="Project 5" />
+				<img src="/img/img5.png" alt="Project 5" />
 			</div>
 			<div class="img-square">
-				<img src="../static/img/img6.png" alt="Project 6" />
+				<img src="/img/img6.png" alt="Project 6" />
 			</div>
 		</div>
 	</div>
