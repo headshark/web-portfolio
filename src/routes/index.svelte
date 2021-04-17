83763ec26288681305c38cb7f371e38daa4c98c3
patrickt <torralbapatrick15@gmail.com>
2021-04-16 21:25:03 -0500

    :rocket: Optimize images

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,22 +8,22 @@
 			class="grid grid-flow-row grid-cols-3 gap-3 mx-4 md:grid-cols-4 md:gap-4 lg:grid-cols-5 lg:gap-5"
 		>
 			<div class="img-square">
-				<img src="/img/img1.png" alt="Project 1" />
+				<img src="{import.meta.env.VITE_IMG_PATH}/project1-1.webp" alt="Project 1" />
 			</div>
 			<div class="img-square">
-				<img src="/img/img2.png" alt="Project 2" />
+				<img src="{import.meta.env.VITE_IMG_PATH}/project1-2.webp" alt="Project 2" />
 			</div>
 			<div class="img-square">
-				<img src="/img/img3.png" alt="Project 3" />
+				<img src="{import.meta.env.VITE_IMG_PATH}/project1-3.webp" alt="Project 3" />
 			</div>
 			<div class="img-square">
-				<img src="/img/img4.png" alt="Project 4" />
+				<img src="{import.meta.env.VITE_IMG_PATH}/project1-4.webp" alt="Project 4" />
 			</div>
 			<div class="img-square">
-				<img src="/img/img5.png" alt="Project 5" />
+				<img src="{import.meta.env.VITE_IMG_PATH}/project1-5.webp" alt="Project 5" />
 			</div>
 			<div class="img-square">
-				<img src="/img/img6.png" alt="Project 6" />
+				<img src="{import.meta.env.VITE_IMG_PATH}/project1-6.webp" alt="Project 6" />
 			</div>
 		</div>
 	</div>
